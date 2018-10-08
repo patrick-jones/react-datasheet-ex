28f84ebe7952c32060b73b2cde8482670a4028ab
Patrick Jones <patrick.jones@amosstrading.com>
2018-10-08 15:20:15 -0500

    [ci skip] Updates

--- dist/lib/stories/store/model.d.ts
+++ dist/lib/stories/store/model.d.ts
@@ -1,4 +1,4 @@
 import { ExampleCellType, ExampleModel, ExampleRow } from './interfaces';
 export declare const model: ExampleModel;
 export declare function rowsToData(rows: ExampleRow[]): ExampleCellType[][];
-export declare function renderValue(cell: ExampleCellType): string | number | null;
+export declare function renderValue(cell: ExampleCellType): import("./interfaces").ExampleCellValue;
